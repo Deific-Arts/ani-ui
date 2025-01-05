192fc6f7b120eb78f2de6b1612f1c65dd87082e7
Hasani Rogers <hasani.rogers@gmail.com>
2025-01-04 23:09:33 -0500

    pwa prep

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite';
 import litCss from 'vite-plugin-lit-css';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [litCss({
-    // your global and rel="stylesheet" styles must be excluded
-    exclude: './src/index.scss'
-  })],
+  plugins: [
+    litCss({
+      // your global and rel="stylesheet" styles must be excluded
+      exclude: './src/index.scss'
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Ani Book Quotes',
+        short_name: 'Ani',
+        description: 'Ani Book Quotes is social media designed for book lovers!',
+        theme_color: '#111827',
+        icons: [
+          {
+            src: 'icons/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+],
   build: {
     target: 'esnext'
   }
