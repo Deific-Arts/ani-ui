04779b53237f53e1b67d7fbe24e7697bf6c003cb
Hasani Rogers <hasani.rogers@gmail.com>
2025-01-13 00:02:42 -0500

    pwa enhancements

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
+        id: 'ani-book-quotes',
         name: 'Ani Book Quotes',
         short_name: 'Ani',
         description: 'Ani Book Quotes is social media designed for book lovers!',
@@ -27,7 +28,40 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png'
           }
-        ]
+        ],
+        launch_handler: {
+          client_mode: ['auto'],
+        },
+        orientation: 'portrait',
+        screenshots: [{
+            src: 'screenshot.png',
+            sizes: '2865x1617',
+            type: 'image/png',
+        }],
+        categories: ['productivity', 'social-networking', 'entertainment', 'literature'],
+        dir: 'ltr',
+        related_applications: [{
+          platform: 'play',
+          url: 'https://play.google.com/store/apps/details?id=me.guyca.kippi&hl=en_US',
+          id: 'me.guyca.kippi',
+        }],
+        'prefer_related_applications': true,
+        scope_extensions: [{ origin: '*.anibookquotes.com' }]
+      },
+      workbox: {
+        runtimeCaching: [{
+          handler: 'NetworkOnly',
+          urlPattern: /\/api\/.*\/*.json/,
+          method: 'POST',
+          options: {
+            backgroundSync: {
+              name: 'aniQueue',
+              options: {
+                maxRetentionTime: 24 * 60
+              }
+            }
+          }
+        }]
       }
     })
 ],
